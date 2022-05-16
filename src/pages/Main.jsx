4b8f9fb77f6305be0a6ea5d6b6201c00d75700a9
elijahsamuels <elijahsamuels@gmail.com>
2022-05-16 15:58:55 -0600

    adding Main page

---
+++ src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Main() {
+	return (
+		<div className="page">
+			<h1>Main</h1>
+			<p>This is a sample page with sample data. The focal point is the navbar and the dropdown menu.</p>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis fugit ipsam, odit labore voluptas iusto dicta ut esse sit quisquam nesciunt veniam culpa sint voluptatibus nostrum aliquid atque quod consequatur!</p>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis fugit ipsam, odit labore voluptas iusto dicta ut esse sit quisquam nesciunt veniam culpa sint voluptatibus nostrum aliquid atque quod consequatur!</p>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis fugit ipsam, odit labore voluptas iusto dicta ut esse sit quisquam nesciunt veniam culpa sint voluptatibus nostrum aliquid atque quod consequatur!</p>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis fugit ipsam, odit labore voluptas iusto dicta ut esse sit quisquam nesciunt veniam culpa sint voluptatibus nostrum aliquid atque quod consequatur!</p>
+		</div>
+	)
+}
+
+export default Main
